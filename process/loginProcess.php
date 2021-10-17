66551f9c6ad379a10101b1170a3af477dca5fade
R3KT19 <batararaja19@gmail.com>
2021-10-17 19:39:00 +0700

    rev

--- process/loginProcess.php
+++ process/loginProcess.php
@@ -1,49 +1,27 @@
 <?php
-    // ini buat ngecek tombol yang namenya 'register' sudah di pencet atau belum
-    // $_POST itu method di formnya
-    if(isset($_POST['login'])){
+    require ('../db.php');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-        include('../db.php'); // untuk mengoneksikan dengan databas dengan memanggil file db.php
+    $sql = "SELECT * FROM users where username = '$username'";
+    $query = mysqli_query($con,$sql);
 
-        //tampung nilai yang ada di from ke variable
-        // sesuaikan variabel name yang ada di registerPage.php disetiap input
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        // Melakukan insert ke databse dengan query dibawah ini
-        $query = mysqli_query($con, "SELECT * FROM users WHERE username = '$username'") or die(mysqli_error($con));
-        // ini buat ngecek kalo misalnya hasil dari querynya == 0 ato ga ketemu -> usernamenya tdk ditemukan
-        if(mysqli_num_rows($query) == 0){
-            echo
-                '<script>
-                alert("Username not found!"); window.location = "../page/loginPage.php"
-                </script>';
-    }else{
-            $user = mysqli_fetch_assoc($query);
-            if(password_verify($password, $user['password'])){
-            // session adalah variabel global sementara yang disimpen di server
-            // buat mulai sessionnya pake session_start()
-            session_start();
-            //isLogin ini temp variable yang gunanya buat ngecek nanti apakah sdh login ato belum
-            $_SESSION['isLogin'] = true;
-            $_SESSION['user'] = $user;
-            echo
-                '<script>
-                alert("Login Success"); window.location = "../page/dashboardPage.php"
-                </script>';
-        }else {
-            echo
-                '<script>
-                alert("Username or Password Invalid");
-                window.location = "../page/loginPage.php"
-                </script>';
+    if(mysqli_num_rows($query) == 0 ){
+        echo "EMAIL TIDAK DITEMUKAN";
+    }else {
+        $user = mysqli_fetch_assoc($query);
+        if(password_verify($password,$user['password'])){
+            if($user['is_verified']==1){
+                session_start();
+                $_SESSION['isLogin'] = true;
+                $_SESSION['user'] = $user;
+    
+                header("location: ../page/dashboardPage.php");
+            }else {
+                echo "VERIF EMAIL DULU GAN !";
             }
+        }else {
+            echo "PASSWORD SALAH !";
+        }
     }
-}else{
-    echo
-        '<script>
-        window.history.back()
-        </script>';
- }
- 
 ?>
